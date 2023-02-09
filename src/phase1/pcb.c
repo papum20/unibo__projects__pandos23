ab74c0c11629e11586e2e9d9854d943ecb9e09f2
Giuseppe <giuseppspathis@gmail.com>
2023-02-09 19:36:47 +0100

    added first function

--- src/phase1/pcb.c
+++ src/phase1/pcb.c
@@ -1,7 +1,9 @@
-#include "pcb.h"
+
+#include "../../include/pcb.h"
+
 
 pcbFree_h;						//list of free or unused PCBs
-pcbFree_table[MAX_PROC]; // PCB array, with max size=MAX_PROC
+pcbFree_table[MAXPROC]; // PCB array, with max size=MAX_PROC
 
 
 //// ALLOCATION
@@ -15,3 +17,6 @@ pcb_t *outChild(pcb_t *p)
 }
 
 //// TREES
+static int emptyChild(pcb_t *p){
+	return (list_empty(&(p->p_child)));
+}

9b408a94d7088a29fdec4fc43dfa8e028ce186c4
papum_debian <danieledugo1@gmail.com>
2023-02-12 18:58:10 +0100

    implementing my pcb functions

--- src/phase1/pcb.c
+++ src/phase1/pcb.c
@@ -13,21 +13,51 @@ void initPcbs()
 {
 	static pcb_t pcbFree_table[MAXPROC];
 
-	INIT_LIST_HEAD(&(pcbFree_h.p_list));
+	INIT_LIST_HEAD(&pcbFree_h.p_list);
 	pcb_t *p_pcb;
 	//reverse for to only evaluate the plus once
 	for(p_pcb = pcbFree_table + MAXPROC - 1; p_pcb >= pcbFree_table; p_pcb--)
-		list_add(&(p_pcb->p_list), &(pcbFree_h.p_list));
+		list_add(&p_pcb->p_list, &pcbFree_h.p_list);
 }
 
 
 //// ALLOCATION
 
-void freePcb(pcb_t *p) {
-	
+void freePcb(pcb_t *p)
+{
+	list_add_tail(&p->p_list, &pcbFree_h.p_list);
 }
 
-pcb_t *allocPcb();
+
+#define __DFLT_LIST_HEAD {NULL, NULL}
+
+/*	helper function for allcPcb,
+	initiates Pcb's values to default.
+*/
+static inline void __initPcb(pcb_t *p)
+{
+    /* process queue  */
+    p->p_list = NULL;
+    /* process tree fields */
+    struct pcb_t    *p_parent; /* ptr to parent	*/
+	p->p_child = __DFLT_LIST_HEAD;
+    p->p_sib = 
+    /* process status information */
+    state_t p_s;    /* processor state */
+	p->p_time = 0;
+    /* Pointer to the semaphore the process is currently blocked on */
+    p->p_semAdd = NULL;
+    /* Namespace list */
+    for()
+}
+
+pcb_t *allocPcb()
+{
+	if(list_empty(&pcbFree_h.p_list))
+		return NULL;
+	
+	__initPcb()
+}
 
 
 

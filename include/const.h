6bfcb89192c39032de1fbc1ab70a421b2ea04c4a
papum_debian <danieledugo1@gmail.com>
2023-02-16 18:08:11 +0100

    HIDDEN comment; documentation structure

--- include/const.h
+++ include/const.h
@@ -2,6 +2,10 @@
 #define PANDOS23_CONST_H
 
 
+/*	alias of static, for disambiguating its use to declare
+	"private" functions, from its use as
+	persistent/global/local-to-translation-unit
+*/
 #define HIDDEN static
 
 

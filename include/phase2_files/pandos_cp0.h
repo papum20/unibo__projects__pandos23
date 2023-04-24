15da1635d6e4d09aad502f789b24e7bb097dd0a9
papum_debian <danieledugo1@gmail.com>
2023-04-24 16:02:18 +0200

    included files

--- include/phase2_files/pandos_cp0.h
+++ include/phase2_files/pandos_cp0.h
@@ -19,6 +19,20 @@
 #define CAUSE_EXCCODE_BIT      2
 #define CAUSE_GET_EXCCODE(x)   (((x) & CAUSE_EXCCODE_MASK) >> CAUSE_EXCCODE_BIT)
 
+/* Exception codes - naming follows standard MIPS mnemonics */
+#define EXC_INT                0
+#define EXC_MOD                1
+#define EXC_TLBL               2
+#define EXC_TLBS               3
+#define EXC_ADEL               4
+#define EXC_ADES               5
+#define EXC_IBE                6
+#define EXC_DBE                7
+#define EXC_SYS                8
+#define EXC_BP                 9
+#define EXC_RI                 10
+#define EXC_CPU                11
+#define EXC_OV                 12
 
 
 
